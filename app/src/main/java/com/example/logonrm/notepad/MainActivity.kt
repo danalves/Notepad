f4115dc55000791fbc1c484b624172d1abdf5d55
Danilo Alves Vieira <vieira.daniloalves@gmail.com>
2018-03-13 15:15:56 -0300

    mudei cor

--- app/src/main/java/com/example/logonrm/notepad/MainActivity.kt
+++ app/src/main/java/com/example/logonrm/notepad/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.logonrm.notepad
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
+import android.widget.EditText
 import android.widget.Toast
+import com.example.logonrm.notepad.R.id.etConteudo
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +17,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater = menuInflater
@@ -35,5 +41,7 @@ class MainActivity : AppCompatActivity() {
             else -> return super.onOptionsItemSelected(item)
         }
     }
+
+
 
 }
